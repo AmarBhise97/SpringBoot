544b717b77c2fcfa5bd109f365db6a349d85658d
Amar bhise <bhiseamarwagholi@gmail.com>
2026-03-18 23:01:22 +0530

    API Gateway

---
+++ APIGateWay1/src/main/java/com/APIGateway1/APIGateWay1/ApiGateWay1Application.java
@@ -0,0 +1,13 @@
+package com.APIGateway1.APIGateWay1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGateWay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGateWay1Application.class, args);
+	}
+
+}
---
+++ APIGateWay1/src/test/java/com/APIGateway1/APIGateWay1/ApiGateWay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIGateway1.APIGateWay1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGateWay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
